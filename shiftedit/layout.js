5569c101216ba375d48a8a582a5564c82d4027c4
Adam Jimenez <adam.jimenez@gmail.com>
2015-11-14 15:19:23 +0000

    fix site menu not overflowing
    and enable live resizing.

--- shiftedit/layout.js
+++ shiftedit/layout.js
@@ -10,17 +10,20 @@ function init() {
     //page layout
     myLayout = $('body').layout({
         resizable: true,
+        //showOverflowOnHover: true,
     	west__size:			300,
     	west__minSize:			300,
     	east__size:			300,
     	south__size:		300,
     	south__initClosed:	true,
+    	livePaneResizing:   true,
     	north: {
             enableCursorHotkey: false,
             closable: false,
             resizable: false,
             spacing_open: 0,
-            spacing_closed: 0
+            spacing_closed: 0,
+            minSize: 34
         },
         stateManagement__enabled:	true
     	// RESIZE Accordion widget when panes resize
@@ -29,11 +32,23 @@ function init() {
     });
 
     myLayout.allowOverflow("north");
+    myLayout.allowOverflow("west");
 
     // if a new theme is applied, it could change the height of some content,
     // so call resizeAll to 'correct' any header/footer heights affected
     // NOTE: this is only necessary because we are changing CSS *AFTER LOADING* using themeSwitcher
-    //setTimeout( myLayout.resizeAll, 1000 ); /* allow time for browser to re-render with new theme */
+    setTimeout( function() {
+        myLayout.resizeAll();
+
+        // some shenanigans to get the resize bars in the right place
+        var eastWidth = myLayout.panes.east.outerWidth();
+        var westWidth = myLayout.panes.west.outerWidth();
+
+        myLayout.sizePane('east', eastWidth+1);
+        myLayout.sizePane('east', eastWidth);
+        myLayout.sizePane('west', westWidth+1);
+        myLayout.sizePane('west', westWidth);
+    }, 1000 ); /* allow time for browser to re-render with new theme */
 
     //send resize events to window
     $('.ui-layout-pane').on('layoutpaneonresize', function() { $(window).trigger('resize'); });
