d713cf901bf86360e96ba82b73cbc2893a8e9dbf
Adam Jimenez <adam.jimenez@gmail.com>
2015-11-18 13:44:13 +0000

    build file

---
+++ build/build.js
@@ -0,0 +1,8 @@
+({
+    mainConfigFile: '../shiftedit/app.js',
+	baseUrl: "../lib",
+	name: '../shiftedit/app',
+    out: "../../build/shiftedit.js",
+    //optimize: 'none',
+    wrapShim: 'true'
+})
