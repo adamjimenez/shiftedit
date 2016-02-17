fe756a717cb57cd9a7e8fc9fd7921738534b0abe
Adam Jimenez <adam.jimenez@gmail.com>
2016-02-17 10:21:01 +0000

    "open file in new browser tab" not working
    fixes #127

--- shiftedit/hash.js
+++ shiftedit/hash.js
@@ -2,45 +2,49 @@ define(['app/tabs', 'app/site'], function (tabs, site) {
 
 var value = '';
 
-load = function () {
+load = function (callback) {
 	var hash = window.location.hash.substr(1);
 	hash = decodeURIComponent(hash);
 
-	if(value === hash){
-		return;
-	}
-
-	console.log('hash: '+ hash);
-
-	//protect from xss
-	if(hash.indexOf('<')!==-1){
-	    return;
-	}
-
-	var line = 0;
-	var files = hash.split('|');
-	files.forEach(function(path){
-		var pos = path.indexOf('/');
-		if (pos !== -1) {
-    		var siteName = path.substr(0, pos);
-    		var file = path.substr(pos + 1);
-
-    		pos = file.indexOf(':');
-    		if(pos!==-1) {
-    			line = file.substr(pos+1);
-    			file = file.substr(0, pos);
-    		}
-
-    		var settings = site.getSettings(siteName);
-	        tabs.open(file, settings.id, function(tab, firstOpen) {
-	        	if(firstOpen) {
-		        	var editor = tabs.getEditor(tab);
-		        	editor.gotoLine(line);
-		        	editor.focus();
-	        	}
-	        });
+	var files = [];
+	if(value !== hash){
+		console.log('hash: '+ hash);
+	
+		//protect from xss
+		if(hash.indexOf('<')!==-1){
+			console.warn('"<" in file name');
+		    return;
 		}
-	});
+	
+		var line = 0;
+		files = hash.split('|');
+		files.forEach(function(path){
+			var pos = path.indexOf('/');
+			if (pos !== -1) {
+	    		var siteName = path.substr(0, pos);
+	    		var file = path.substr(pos + 1);
+	
+	    		pos = file.indexOf(':');
+	    		if(pos!==-1) {
+	    			line = file.substr(pos+1);
+	    			file = file.substr(0, pos);
+	    		}
+	
+	    		var settings = site.getSettings(siteName);
+		        tabs.open(file, settings.id, function(tab, firstOpen) {
+		        	if(firstOpen) {
+			        	var editor = tabs.getEditor(tab);
+			        	editor.gotoLine(line);
+			        	editor.focus();
+		        	}
+		        });
+			}
+		});
+	}
+
+    if (callback) {
+    	callback(files);
+    }
 };
 
 set = function(hash) {
@@ -50,9 +54,9 @@ set = function(hash) {
     	console.log('set hash: #'+ value);
         window.location.hash = '#'+value;
     }
-}
+};
 
-$(window).on( 'hashchange', load );
+$(window).on( 'hashchange', function(e) { load(); } );
 
 return {
     load: load,
--- shiftedit/main.js
+++ shiftedit/main.js
@@ -42,17 +42,17 @@ define(['exports',"jquery-ui","app/lang","app/prefs","app/tabs","app/layout","ap
         splash.update('loading sites..');
         site.load()
         .done(function() {
-            if(prefs.restoreTabs) {
-                require('app/restore').restoreBatch(preferences.getOpeningFilesBatch(), function() {
-                	$( ".ui-layout-east, .ui-layout-center, .ui-layout-south" ).each(function( index ) {
-                		if (!$( this ).children('.ui-tabs-nav').children(':not(.button)').length) {
-                			$( this ).tabs('add');
-                		}
-                	} );
-                });
-            }
-
-            var hash = require("app/hash").load();
+       		var hash = require("app/hash").load(function(files) {
+	            if(prefs.restoreTabs && !files.length) {
+	                require('app/restore').restoreBatch(preferences.getOpeningFilesBatch(), function() {
+	                	$( ".ui-layout-east, .ui-layout-center, .ui-layout-south" ).each(function( index ) {
+	                		if (!$( this ).children('.ui-tabs-nav').children(':not(.button)').length) {
+	                			$( this ).tabs('add');
+	                		}
+	                	} );
+	                });
+	            }
+    		});
         });
 
         var recent = require("app/recent");
