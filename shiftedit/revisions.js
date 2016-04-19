32e2aa1fe5f59a023a6bd346c1fdd0e8a834d628
Adam Jimenez <adam.jimenez@gmail.com>
2016-04-19 18:45:44 +0100

    fix revision dialog layout issues

--- shiftedit/revisions.js
+++ shiftedit/revisions.js
@@ -40,8 +40,8 @@ function open() {
     //revisions dialog
     $( "body" ).append('<div id="dialog-revisions" title="Revisions">\
         <div class="revisions">\
-            <p><select id="revisionFile" class="ui-widget ui-state-default ui-corner-all"></select></p>\
-            <p><select id="revision" size="20" class="ui-widget ui-state-default ui-corner-all"></select></p>\
+            <p class="hbox"><select id="revisionFile" class="ui-widget ui-state-default ui-corner-all"></select></p>\
+            <p class="hbox"><select id="revision" size="20" class="ui-widget ui-state-default ui-corner-all flex"></select></p>\
         </div>\
         <div id="revisionDiff">\
         </div>\
