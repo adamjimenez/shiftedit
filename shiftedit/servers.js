2cff42688de5ec780fa6275d280a45d24ccb152c
Adam Jimenez <adam.jimenez@gmail.com>
2016-11-04 16:05:39 +0000

    use digitalocean oauth

--- shiftedit/servers.js
+++ shiftedit/servers.js
@@ -1,9 +1,10 @@
-define(['app/config', 'app/loading', 'app/util', 'app/prefs', 'app/lang', 'app/prompt', 'app/ssh', 'jquery'], function (config, loading, util, preferences, lang, prompt, ssh) {
+define(['app/config', 'app/loading', 'app/util', 'app/prefs', 'app/prompt', 'app/ssh', 'app/lang', 'jquery'], function (config, loading, util, preferences, prompt, ssh) {
 
 var selected;
 var servers = [];
 var settings = {};
 var currentServer;
+var lang = require('app/lang').lang;
 
 function load() {
 	loading.fetch(config.apiBaseUrl+'servers', {
@@ -64,11 +65,15 @@ function edit(add) {
 				<span id="providerRadio">\
 					<input type="radio" name="provider" value="AWS" id="providerRadio1">\
 					<label for="providerRadio1">\
-						<img alt="Amazon Web Services" src="https://shiftedit.s3.amazonaws.com/images/logos/aws.svg" height="64">\
+						<img alt="Amazon Web Services" src="https://shiftedit.s3.amazonaws.com/images/logos/aws.svg" height="32">\
 					</label>\
-					<input type="radio" name="provider" value="Linode" id="providerRadio2">\
+					<input type="radio" name="provider" value="DigitalOcean" id="providerRadio2">\
 					<label for="providerRadio2">\
-						<img alt="Linode" src="https://shiftedit.s3.amazonaws.com/images/logos/linode.svg" height="64">\
+						<img alt="Digital Ocean" src="https://shiftedit.s3.amazonaws.com/images/logos/digitalocean.svg" height="32">\
+					</label>\
+					<input type="radio" name="provider" value="Linode" id="providerRadio3">\
+					<label for="providerRadio3">\
+						<img alt="Linode" src="https://shiftedit.s3.amazonaws.com/images/logos/linode.svg" height="32">\
 					</label>\
 				</span>\
 			</p>\
@@ -96,10 +101,14 @@ function edit(add) {
 			$('#server_pass_container').show();
 			$('#username').attr('placeholder', 'Access Key ID');
 			$('#password').attr('placeholder', 'Secret Access Key');
+		} else if (this.value==='DigitalOcean') {
+			$('#server_user_container').hide();
+			$('#server_pass_container').hide();
+			//$('#password').attr('placeholder', 'Token');
 		} else if (this.value==='Linode') {
 			$('#server_user_container').hide();
 			$('#server_pass_container').show();
-			$('#username').attr('placeholder', 'Api Key');
+			$('#password').attr('placeholder', 'Api Key');
 		} else {
 			$('#server_user_container').hide();
 			$('#server_pass_container').hide();
@@ -262,7 +271,7 @@ function open() {
 		close: function( event, ui ) {
 			$( this ).remove();
 		},
-		width: 400,
+		width: 420,
 		height: 300,
 		buttons: {
 			"Add Server": function() {
@@ -364,95 +373,107 @@ function save() {
 	ajax.then(function (data) {
 		loading.stop();
 
-		if(data.success){
-			var finish = function() {
-				currentServer = serverId;
-				load();
-	
-				$( "#dialog-server" ).dialog( "close" );
-				$( "#dialog-server" ).remove();
-			};
-			
-			serverId = data.server;
-			if (!serverId) {
-				console.log('no server id');
-				return false;
-			}
-			
-			$('#serverSettings [name=id]').val(serverId);
-			
-			var status = '';
-			var ajax;
-			var check_status = function() {
-				ajax = $.ajax({
-					url: config.apiBaseUrl+'servers?cmd=get_status&server='+serverId,
-					method: 'GET',
-					dataType: 'json',
-					data: {
-						status: status
-					}
-				}).then(function (data) {
-					loading.stop();
-					
-					if(data.success) {
-						status = data.status;
+		if(data.success) {
+			if (data.require_auth) {
+				switch(params.provider) {
+					case 'DigitalOcean':
+						window.open('/account/services/digitalocean');
+					break;
+					default:
+						console.error('invalid provider' + params.provider);
+					break;
+				}
+			} else {
+				var finish = function() {
+					currentServer = serverId;
+					load();
+		
+					$( "#dialog-server" ).dialog( "close" );
+					$( "#dialog-server" ).remove();
+				};
+				
+				serverId = data.server;
+				if (!serverId) {
+					console.log('no server id');
+					return false;
+				}
+				
+				$('#serverSettings [name=id]').val(serverId);
+				
+				var status = '';
+				var ajax;
+				var check_status = function() {
+					ajax = $.ajax({
+						url: config.apiBaseUrl+'servers?cmd=get_status&server='+serverId,
+						method: 'GET',
+						dataType: 'json',
+						data: {
+							status: status
+						}
+					}).then(function (data) {
+						loading.stop();
 						
-						if (status==='ready') {
+						if(data.success) {
+							status = data.status;
+							
+							if (status==='ready') {
+								finish();
+							} else {
+								check_status();
+							}
+						} else {
+							prompt.alert({title:'Error', msg:data.error});
+							return;
+						}
+					});
+					
+					loading.start(status, function(){
+						console.log('abort server status');
+						console.log(ajax);
+						ajax.abort();
+					}, true);
+				};
+				
+				if (wait) {
+					var source = new EventSource(config.apiBaseUrl+'servers?cmd=create&server='+serverId, {withCredentials: true});
+					var abortFunction = function(){
+						if( source ){
+							source.close();
+						}
+					};
+				
+					var stopLoading = true;
+					source.addEventListener('message', function(event) {
+						//console.log(event.data);
+						var result = JSON.parse(event.data);
+						var msg = result.msg;
+	
+						loading.stop(false);
+						
+						if (msg.substr(0, 6)==='Error:') {
+							prompt.alert({title:'Error', msg: msg});
+						} else if (msg==='waiting for server') {
+							stopLoading = false;
+							check_status();
+						} else if (msg==='ready') {
+							stopLoading = false;
 							finish();
 						} else {
-							check_status();
+							loading.start(msg, abortFunction, true);
 						}
-					} else {
-						prompt.alert({title:'Error', msg:data.error});
-						return;
-					}
-				});
+					}, false);
 				
-				loading.start(status, function(){
-					console.log('abort server status');
-					ajax.abort();
-				}, true);
-			};
-			
-			if (wait) {
-				var source = new EventSource(config.apiBaseUrl+'servers?cmd=create&server='+serverId, {withCredentials: true});
-				var abortFunction = function(){
-					if( source ){
-						source.close();
-					}
-				};
-			
-				var stopLoading = true;
-				source.addEventListener('message', function(event) {
-					//console.log(event.data);
-					var result = JSON.parse(event.data);
-					var msg = result.msg;
-
-					loading.stop(false);
-					
-					if (msg.substr(0, 6)==='Error:') {
-						prompt.alert({title:'Error', msg: msg});
-					} else if (msg==='waiting for server') {
-						stopLoading = false;
-						check_status();
-					} else if (msg==='ready') {
-						stopLoading = false;
-						finish();
-					} else {
-						loading.start(msg, abortFunction, true);
-					}
-				}, false);
-			
-				source.addEventListener('error', function(event) {
-					if (stopLoading) {
-						loading.stop(false);
-					}
-					if (event.eventPhase == 2) { //EventSource.CLOSED
-						abortFunction();
-					}
-				}, false);
-			} else {
-				finish();
+					source.addEventListener('error', function(event) {
+						if (stopLoading) {
+							loading.stop(false);
+						}
+						if (event.eventPhase == 2) { //EventSource.CLOSED
+							abortFunction();
+						}
+					}, false);
+				} else {
+					finish();
+				}
 			}
 		}else{
 			var error = 'unknown';
@@ -467,7 +488,7 @@ function save() {
 	});
 }
 
-$('body').on('click', '#servers a', function(e){
+$('body').on('click', '#servers a, .newTab .server', function(e){
 	open();
 });
 
