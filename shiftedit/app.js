668b993cc07679f28afa6c698c3835d6f12813d2
Adam Jimenez <adam.jimenez@gmail.com>
2018-01-31 11:36:31 +0000

    update dependencies

--- shiftedit/app.js
+++ shiftedit/app.js
@@ -4,37 +4,65 @@
 // except for 'app' ones, which are in a sibling
 // directory.
 requirejs.config({
-	baseUrl: "lib",
+	baseUrl: "",
 	paths: {
-		"json": 'require/json',
-		"locale": '../locale',
-		"app": "../shiftedit",
-		"jquery.contextMenu": "contextMenu/jquery.contextMenu",
-		"jquery.menubar": "menubar/jquery.menubar",
-		"ui.combobox": "combobox/ui.combobox",
-		"ui.basicMenu": "ui.basicMenu/ui.basicMenu",
-		"jstree": "jstree/jstree",
-		"ace": "ace.20161203/src",
-		//"ace/split": 'ace.20151029/src/ext-split',
-		"ace/ext/language_tools": "ace.20161203/src/ext-language_tools",
-		"ace/autocomplete": "ace.20161203/src/ext-tern",
-		"ace/mode/css/csslint": "ace.20161203/src/worker-css",
-		"ace/ext/searchbox": "ace.20161203/src/ext-searchbox",
-		//"ace/ext/emmet": 'ace.20161203/src/ext-emmet',
-		//"firepad": 'firepad/firepad',
-		"firepad-userlist": 'firepad/firepad-userlist',
-		"jsdiff": 'jsdiff/diff',
-		"ui.tabs.overflowResize": 'ui.tabs.overflowResize/ui.tabs.overflowResize',
-		"ace/keyboard/vim": "ace.20161203/src/keybinding-vim",
-		"ace/keyboard/emacs": "ace.20161203/src/keybinding-emacs",
-		"ace/ext/whitespace": "ace.20161203/src/ext-whitespace",
-		"ace/ext/tern": "ace.20161203/src/ext-tern",
-		'beautify': 'js-beautify/beautify',
-		'beautify-css': 'js-beautify/beautify-css',
-		'beautify-html': 'js-beautify/beautify-html',
-		'linkify-html': 'linkify/linkify-html'
+		'firepad': 'node_modules/firepad/dist/firepad',
+		'firebase': 'node_modules/firebase/firebase',
+		"jstree": "node_modules/jstree/dist/jstree",
+		'jquery': 'node_modules/jquery/dist/jquery',
+		'xterm': 'node_modules/xterm/dist/xterm',
+		'addons/attach/attach': 'node_modules/xterm/dist/addons/attach/attach',
+		'addons/fit/fit': 'node_modules/xterm/dist/addons/fit/fit',
+		"jquery.contextMenu": "node_modules/jquery-contextmenu/dist/jquery.contextMenu",
+		'beautify': 'node_modules/js-beautify/js/lib/beautify',
+		'beautify-css': 'node_modules/js-beautify/js/lib/beautify-css',
+		'beautify-html': 'node_modules/js-beautify/js/lib/beautify-html',
+		'markdown-it': 'node_modules/markdown-it/dist/markdown-it',
+		'resumable': 'node_modules/resumablejs/resumable',
+		'lzma': 'node_modules/lzma/src',
+		'jquery-ui': 'node_modules/jquery-ui-bundle/jquery-ui',
+		'uglify': 'node_modules/uglify-js/lib',
+		"diff": 'node_modules/diff/dist/diff',
+		'autosize': 'node_modules/autosize/dist/autosize',
+		'md5': 'node_modules/blueimp-md5/js/md5',
+		"ace": "node_modules/ace-builds/src",
+		"ace/keyboard/emacs": "node_modules/ace-builds/src/keybinding-emacs",
+		"ace/keyboard/vim": "node_modules/ace-builds/src/keybinding-vim",
+		"ace/ext/language_tools": "node_modules/ace-builds/src/ext-language_tools",
+		"ace/mode/css/csslint": "node_modules/ace-builds/src/worker-css",
+		"ace/ext/searchbox": "node_modules/ace-builds/src/ext-searchbox",
+		"ace/ext/whitespace": "node_modules/ace-builds/src/ext-whitespace",
+		'linkify': 'node_modules/linkifyjs/dist/linkify',
+		'linkify-html': 'node_modules/linkifyjs/dist/linkify-html',
+		'text': 'node_modules/text/text',
+		"json": 'node_modules/requirejs-plugins/src/json',
+		'aes': 'node_modules/crypto-libraries/aes',
+		
+		// these ones below should be moved to node_modules
+		'diff2html': 'lib/diff2html/diff2html',
+		'diff2html-ui': 'lib/diff2html/diff2html-ui',
+		'cssmin': 'lib/cssmin/cssmin',
+		'coffee-script': 'lib/coffee-script/coffee-script',
+		'emmet': 'lib/emmet/emmet',
+		'jquery.layout': 'lib/layout/jquery.layout',
+		'jstreetable': 'lib/jstreetable/jstreetable',
+		"jquery.menubar": "lib/menubar/jquery.menubar",
+		"ui.basicMenu": "lib/basicMenu/ui.basicMenu",
+		'ui.tabs.addTab': 'lib/addTab/ui.tabs.addTab',
+		'ui.tabs.closable': 'lib/closable/ui.tabs.closable',
+		"ui.combobox": "lib/combobox/ui.combobox",
+		"ui.tabs.overflowResize": 'lib/overflowResize/ui.tabs.overflowResize',
+		"firepad-userlist": 'lib/firepad-userlist/firepad-userlist',
+		"ace/autocomplete": "lib/tern/ext-tern",
+		"ace/ext-emmet": "lib/emmet/ext-emmet",
+		"ace/ext/tern": "lib/tern/ext-tern",
+		
+		"app": "shiftedit"
 	},
 	"shim": {
+		"app/main": {
+			deps: ['ace/ace']
+		},
 		"ace/ext-emmet": {
 			deps: ['ace/ace','emmet']
 		},
@@ -90,11 +118,15 @@ requirejs.config({
 			exports: "$",
 			deps: ["jquery"]
 		},
-		"firepad/firepad": {
-			deps: ['ace/ace', 'firepad/firebase']
+		'firebase': {
+			exports: 'firebase'
+		},
+		"firepad": {
+			deps: ['ace/ace', 'firebase']
 		},
 		"firepad-userlist": {
-			deps: ['firepad/firebase']
+			exports: "FirepadUserList",
+			deps: ['firebase']
 		},
 		"app/ssh": {
 			deps: [
@@ -108,7 +140,17 @@ requirejs.config({
 		},
 		"linkify-html": {
 			deps: [
-				'linkify/linkify'
+				'linkify'
+			]
+		},
+		"diff2html": {
+			deps: [
+				'diff'
+			]
+		},
+		"diff2html-ui": {
+			deps: [
+				'diff2html'
 			]
 		},
 		/*
